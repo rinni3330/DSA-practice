2959a801fe9da0abbf7a74a2b922e3bc5a89c9bd
rinni3330 <kolarasagnasrinivas141@gmail.com>
2026-03-29 13:01:03 +0530

    climb stairs using dynamic programming

---
+++ Dynamic_Programming/climb_stairs.py
@@ -0,0 +1,14 @@
+def climb_stairs(n):
+    if n <= 2:
+        return n
+
+    dp = [0]*(n+1)
+    dp[1], dp[2] = 1, 2
+
+    for i in range(3,n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+
+    return dp[n]
+
+n = int(input("Enter number: "))
+print(climb_stairs(n))
